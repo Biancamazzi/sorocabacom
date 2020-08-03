6f0b021b5e3c2a1eb1cf0cb55a1f4a1044964177
Bianca Mazzi <bianca.mazzi@hotmail.com>
2020-08-03 20:57:01 -0300

    inserido carrossel e atualizacao de style + resposividade

--- main.js
+++ main.js
@@ -5,3 +5,42 @@ function Enviar() {
         <h3>${nome}, formulário enviado com sucesso!</h3>
     `)
 }
+
+$('#personagem-carrosel').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: `
+    <div class="slick-prev col-1 arrow">
+        <div class="seta"><</div>
+    </div>
+    `,
+    nextArrow: `
+    <div class="slick-next col-1 arrow">
+      <div class="seta">></div>
+    </div>`,
+    responsive: [
+        {
+            breakpoint: 1050,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true,
+                arrows: false,
+                swipeToSlide: true
+            }
+        },
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true,
+            arrows: false,
+            swipeToSlide: true
+          }
+        }
+    ]
+});
